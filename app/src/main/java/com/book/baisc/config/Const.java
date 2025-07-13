a562b20948e7f1d03a0ca33a3581f086fbadb96f
wd <2393146578@qq.com>
2025-07-13 21:47:07 +0800

    OCT 发布前的更改

--- app/src/main/java/com/book/baisc/config/Const.java
+++ app/src/main/java/com/book/baisc/config/Const.java
@@ -3,7 +3,7 @@ package com.book.baisc.config;
 public class Const {
     public static final String XHS_PACKAGE = "com.xingin.xhs";
 
-    public final static int CASUAL_LIMIT_COUNT = 10;
+    public final static int CASUAL_LIMIT_COUNT = 3;
     public static boolean MOTIVATE_CHANGE = false;
 
     public static String lastDetectedInterface = ""; // 缓存上次检测的界面类型
